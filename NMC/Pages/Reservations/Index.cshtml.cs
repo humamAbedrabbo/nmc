e938823dd899dbf9ea40251231098bc18a2be0b4
Humam <humam.abedrabbo@hotmail.com>
2020-12-08 20:35:45 +0200

    Upgrade to Net5

--- NMC/Pages/Reservations/Index.cshtml.cs
+++ NMC/Pages/Reservations/Index.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Localization;
 using NMC.Models;
-using NMC.Resources;
+
 using NMC.Services;
 
 namespace NMC.Pages.Reservations
