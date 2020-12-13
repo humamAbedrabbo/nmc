ffe7e577a86755260803ab83692807600eb385b5
Humam <humam.abedrabbo@hotmail.com>
2020-12-13 03:53:48 +0200

    Add Migrations

---
+++ NMC/Migrations/20201213012221_Create_Patient_Schema.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace NMC.Migrations
+{
+    public partial class Create_Patient_Schema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    FatherName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    MotherName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Gender = table.Column<int>(type: "integer", nullable: false),
+                    MaritalStatus = table.Column<int>(type: "integer", nullable: false),
+                    Children = table.Column<int>(type: "integer", nullable: false),
+                    IdentityType = table.Column<int>(type: "integer", nullable: false),
+                    IdentityNo = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Phone = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: true),
+                    Mobile = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: true),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Address = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Occupation = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    BloodType = table.Column<int>(type: "integer", nullable: false),
+                    RH = table.Column<int>(type: "integer", nullable: false),
+                    PhotoUrl = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "7ecf9538-e53f-42ea-8dad-361a308434fb", "AQAAAAEAACcQAAAAEG9+bbtaqNNkYDAgmp+HY7Mxb16zxMlPK8gSQVHaHuRKIGsqZxpJwzfXFBf/0/pjyA==", "57b61596-4671-4fc7-9356-c6e5ef8e9d67" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patients");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "37d7655a-cae2-48cc-9829-83031552eb0f", "AQAAAAEAACcQAAAAEHaAFNJkrr77uahboCzx/CyodFG7lutM5kxbPZ6Zez+P9U+WNX/kk0mIXYu6cUJfSA==", "27760718-b76c-4b8a-86a3-37cfd02547dc" });
+        }
+    }
+}
