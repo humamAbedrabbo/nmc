3727571b973244463afa0847154cec44a26dd2c6
Humam <humam.abedrabbo@hotmail.com>
2020-12-13 13:28:19 +0200

    Add Migrations

---
+++ NMC/Migrations/20201213104824_Create_Booking_Schema.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace NMC.Migrations
+{
+    public partial class Create_Booking_Schema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    DoctorId = table.Column<int>(type: "integer", nullable: true),
+                    PatientFirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    PatientLastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Phone = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: true),
+                    Gender = table.Column<int>(type: "integer", nullable: false),
+                    WardId = table.Column<int>(type: "integer", nullable: false),
+                    Notes = table.Column<string>(type: "text", nullable: true),
+                    DownPayment = table.Column<decimal>(type: "numeric", nullable: false),
+                    AmountReceived = table.Column<decimal>(type: "numeric", nullable: false),
+                    From = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    To = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    ValidUntil = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ConfirmationDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Wards_WardId",
+                        column: x => x.WardId,
+                        principalTable: "Wards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "3de8b962-f989-49e2-8170-0a4603536d80", "AQAAAAEAACcQAAAAEEVOc8Gg4utYLKY/6wMXmqyMuGTrxsoVNAtTUXfCwcgeA9AzBq8fSaxd8uwZChTaXw==", "d07b5b14-9575-4897-a233-f94c1cf0d901" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_DoctorId",
+                table: "Bookings",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_WardId",
+                table: "Bookings",
+                column: "WardId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "7ecf9538-e53f-42ea-8dad-361a308434fb", "AQAAAAEAACcQAAAAEG9+bbtaqNNkYDAgmp+HY7Mxb16zxMlPK8gSQVHaHuRKIGsqZxpJwzfXFBf/0/pjyA==", "57b61596-4671-4fc7-9356-c6e5ef8e9d67" });
+        }
+    }
+}
