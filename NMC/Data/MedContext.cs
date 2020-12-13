f5ff397c636451a75e1f6173fe53b7d96d26c312
Humam <humam.abedrabbo@hotmail.com>
2020-12-13 03:53:38 +0200

    Configure Patient in DbContext

--- NMC/Data/MedContext.cs
+++ NMC/Data/MedContext.cs
@@ -20,6 +20,7 @@ namespace NMC.Data
         // Master Domain Entities
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
 
         // Master Types
         public DbSet<RoomGrade> RoomGrades { get; set; }
